f0c6fa3dc7f35163b4006d402b262e2d90641e64
Cheatcodek <theender719@gmail.com>
2018-11-18 21:16:59 -0600

    Renamed yote to lesser yote

---
+++ Items/Ranged/TheLesserYote.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Syntation.Items.Ranged
+{
+    public class TheLesserYote : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Lesser Yote");
+            Tooltip.SetDefault("Past tense of Yeet");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 24;
+            item.ranged = true;
+            item.width = 40;
+            item.height = 20;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 5;
+            item.noMelee = true; 
+            item.knockBack = 6;
+            item.value = 10000;
+            item.rare = 3;
+            item.UseSound = SoundID.Item11;
+            item.autoReuse = true;
+            item.shoot = 12; 
+            item.shootSpeed = 8f;
+            item.useAmmo = AmmoID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
