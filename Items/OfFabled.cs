bfc29a17d48a811201822412fcb1919498d7081f
Cheatcodek <theender719@gmail.com>
2018-11-26 23:34:18 -0600

    Added 1 npc and 1 item
    Added Of Fabled
     -Mystical material that will be used later
     -For now is(?) dropped by the mindscrew
    Added Mindscrew
     -An attempt to screw with the golden spiral and spread it across the canvas, making a somewhat eldritch horrer enemy
     -Instead it just has two frame for some reason (Maybe because I imported demon eye AI?)
     -Floats around like demon eyes
     -Drops Of Fabled

---
+++ Items/OfFabled.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Syntation.Items
+{
+    public class OfFabled : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Of Fabled");
+            Tooltip.SetDefault("A material found only in bizarre places \nSome say it can make untold amounts of glorious weaponry \nOthers know it is a volatile and strange material");
+
+        }
+
+    }
+}
