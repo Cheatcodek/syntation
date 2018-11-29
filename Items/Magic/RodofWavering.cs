324a1046ca9fcb2cc46e03db8b06a8a503f24f79
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2018-11-29 09:45:31 -0600

    Added rod of wavering and a projectile

---
+++ Items/Magic/RodofWavering.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Timers;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Syntation.Items.Magic
+{
+    class RodofWavering : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Rod of Wavering");
+
+        }
+
+
+
+    }
+}
